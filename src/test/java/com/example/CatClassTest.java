35c27ee0e15fcf62811d79e6dae754448e80a44b
Julia <samoletsk8@yandex.ru>
2026-02-14 10:38:19 +0300

    [Fix] add Mock

--- src/test/java/com/example/CatClassTest.java
+++ src/test/java/com/example/CatClassTest.java
@@ -3,8 +3,8 @@ package com.example;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CatClassTest {
     Cat cat;
 
-    @Spy
+    @Mock
     Feline feline;
 
     @BeforeEach
